5e6b9041f4f00ac9a0ad7325239cac3f292755a8
Harrison Malone <harrison.malone2@gmail.com>
2021-01-19 12:25:37 +1100

    added update route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Route, Switch } from "react-router-dom";
 import { Subscriptions } from "./Subscriptions";
 import { NewSubscription } from "./NewSubscription";
 import { Subscription } from "./Subscription";
+import { UpdateSubscription } from '../components/UpdateSubscription'
 import { Container, Nav, RouterLink } from "../styles/App";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route exact path={["/", "/subscriptions"]} component={Subscriptions} />
         <Route exact path="/subscriptions/new" component={NewSubscription} />
         <Route exact path="/subscriptions/:id" component={Subscription} />
+        <Route exact path="/subscriptions/:id/edit" component={UpdateSubscription} />
       </Switch>
     </Container>
   );
