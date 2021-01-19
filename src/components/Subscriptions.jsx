a6fc1c130cd77438a386236fef6bfb80e01b9c14
Harrison Malone <harrison.malone2@gmail.com>
2021-01-19 12:24:52 +1100

    styled components redone to make flexbox work, edit and delete links added along with delete event handler

--- src/components/Subscriptions.jsx
+++ src/components/Subscriptions.jsx
@@ -1,16 +1,39 @@
 import React, { useState, useEffect } from "react";
-import { Card, CardWrapper } from "../styles/Subscriptions";
-import { Link } from "react-router-dom";
+import { OuterCard, InnerCard, CardWrapper, CardLink } from "../styles/Subscriptions";
 
 export function Subscriptions() {
   const [subscriptions, setSubscriptions] = useState([]);
 
-  useEffect(() => {
+  function fetchSubscriptions() {
     // http://localhost:3000/subscriptions
     fetch(`${process.env.REACT_APP_BACKEND_URL}/subscriptions`)
       .then((res) => res.json())
       .then((body) => setSubscriptions(body));
+  }
+
+  useEffect(() => {
+    fetchSubscriptions();
   }, []);
+
+  async function onDeleteLinkClick(e, subscription) {
+    try {
+      e.preventDefault();
+      if (window.confirm("Would you like to delete?")) {
+        await fetch(
+          `${process.env.REACT_APP_BACKEND_URL}/subscriptions/${subscription.id}`,
+          {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        fetchSubscriptions();
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
 
   return (
     <div>
@@ -18,12 +41,21 @@ export function Subscriptions() {
       <CardWrapper>
         {subscriptions.map((subscription) => {
           return (
-            <Card key={subscription.id}>
-              <h2>Name: {subscription.name}</h2>
-              <p>Price: ${subscription.price_per_month}</p>
-              <p>Billing period: {subscription.billing_period}</p>
-              <Link to={`/subscriptions/${subscription.id}`}>Show</Link>
-            </Card>
+            <OuterCard key={subscription.id}>
+              <InnerCard>
+                <h2>Name: {subscription.name}</h2>
+                <p>Price: ${subscription.price_per_month}</p>
+                <p>Billing period: {subscription.billing_period}</p>
+                <CardLink to={`/subscriptions/${subscription.id}`}>Show</CardLink>
+                <CardLink
+                  onClick={(e) => onDeleteLinkClick(e, subscription)}
+                  to={`/subscriptions/${subscription.id}`}
+                >
+                  Delete
+                </CardLink>
+                <CardLink to={`/subscriptions/${subscription.id}/edit`}>Edit</CardLink>
+              </InnerCard>
+            </OuterCard>
           );
         })}
       </CardWrapper>
