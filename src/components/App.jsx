f0b516fe97b72dd3010b147ea11aefeef38495d1
Harrison Malone <harrison.malone2@gmail.com>
2021-01-20 11:56:27 +1100

    removed navbar and added protected route logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,26 @@ import { Route, Switch } from "react-router-dom";
 import { Subscriptions } from "./Subscriptions";
 import { NewSubscription } from "./NewSubscription";
 import { Subscription } from "./Subscription";
-import { UpdateSubscription } from '../components/UpdateSubscription'
-import { Container, Nav, RouterLink } from "../styles/App";
+import { UpdateSubscription } from "../components/UpdateSubscription";
+import { Container } from "../styles/App";
+import { ProtectedRoute } from './ProtectedRoute'
+import { Login } from './Login'
+import { SignUp } from './SignUp'
 
 function App() {
   return (
     <Container>
-      <Nav>
-        <RouterLink to="/subscriptions">Home</RouterLink>
-        <RouterLink to="/subscriptions/new">New Subscription</RouterLink>
-      </Nav>
       <Switch>
-        <Route exact path={["/", "/subscriptions"]} component={Subscriptions} />
-        <Route exact path="/subscriptions/new" component={NewSubscription} />
-        <Route exact path="/subscriptions/:id" component={Subscription} />
-        <Route exact path="/subscriptions/:id/edit" component={UpdateSubscription} />
+        <ProtectedRoute exact path={["/", "/subscriptions"]} component={Subscriptions} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/sign-up" component={SignUp} />
+        <ProtectedRoute exact path="/subscriptions/new" component={NewSubscription} />
+        <ProtectedRoute exact path="/subscriptions/:id" component={Subscription} />
+        <ProtectedRoute
+          exact
+          path="/subscriptions/:id/edit"
+          component={UpdateSubscription}
+        />
       </Switch>
     </Container>
   );
