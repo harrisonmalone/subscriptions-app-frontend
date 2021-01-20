953de0b660511bf3ba743d1faf08630509e89827
Harrison Malone <harrison.malone2@gmail.com>
2021-01-20 11:57:27 +1100

    login and sign up components which make fetch request to rails and add jwt to local storage

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { LoggedOutNavbar } from './LoggedOutNavbar'
+
+export function SignUp({ history }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function onFormSubmit(event) {
+    event.preventDefault();
+    const body = { user: { email, password } };
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_BACKEND_URL}/sign-up`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(body),
+        }
+      );
+      if (response.status >= 400) {
+        throw new Error("incorrect credentials");
+      } else {
+        const response = await fetch( `${process.env.REACT_APP_BACKEND_URL}/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ auth: { email, password } }),
+        });
+        const { jwt } = await response.json();
+        localStorage.setItem("token", jwt);
+        history.push("/subscriptions");
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  return (
+    <>
+      <LoggedOutNavbar />
+      <h1>Sign Up</h1>
+      <form onSubmit={onFormSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  );
+}
