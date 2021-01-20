07f2fa67be9f656b5dbf3939487dec018d57a424
Harrison Malone <harrison.malone2@gmail.com>
2021-01-20 12:07:27 +1100

    fixed fetch that wasn't getting env backend url

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -9,7 +9,7 @@ export function ProtectedRoute({ exact, path, component }) {
   useEffect(() => {
     async function checkAuthStatus() {
       try {
-        const response = await fetch("http://localhost:3000/status", {
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/status`, {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
